a104f1c4c2b816b3aa623171adf0faa229149d56
sutouch08 <sutouch08@gmail.com>
2024-04-16 22:08:49 +0700

    add product collection

--- application/views/masters/product_collection/product_collection_add.php
+++ application/views/masters/product_collection/product_collection_add.php
@@ -18,7 +18,7 @@
 	<div class="form-group">
     <label class="col-sm-3 control-label no-padding-right">Code</label>
     <div class="col-xs-12 col-sm-3">
-      <input type="text" name="code" id="code" class="width-100 r" maxlength="20" value="" autofocus required />
+      <input type="text" name="code" id="code" class="width-100 r" onkeyup="validCode(this)" maxlength="20" value="" autofocus required />
     </div>
     <div class="help-block col-xs-12 col-sm-reset inline red e" id="code-error"></div>
   </div>
