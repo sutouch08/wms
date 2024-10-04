d27eb25d352ef4e5b5f296331dce8cc2f1d52b90
sutouch08 <sutouch08@gmail.com>
2024-10-04 14:45:42 +0700

    consign_check listing

--- application/views/inventory/consign_check/consign_check_list.php
+++ application/views/inventory/consign_check/consign_check_list.php
@@ -1,12 +1,12 @@
 <?php $this->load->view('include/header'); ?>
 <div class="row">
-  <div class="col-sm-6 padding-5">
-    <h3 class="title"><?php echo $this->title; ?></h3>
+  <div class="col-lg-6 col-md-6 col-sm-6 col-sm-12 padding-5">
+    <h4 class="title"><?php echo $this->title; ?></h4>
   </div>
-  <div class="col-sm-6 padding-5">
+  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding-5">
     <p class="pull-right top-p">
       <?php if($this->pm->can_add) : ?>
-        <button type="button" class="btn btn-sm btn-success" onclick="goAdd()"><i class="fa fa-plus"></i> เพิ่มใหม่</button>
+        <button type="button" class="btn btn-xs btn-success" onclick="goAdd()"><i class="fa fa-plus"></i> เพิ่มใหม่</button>
       <?php endif; ?>
     </p>
   </div>
@@ -14,19 +14,19 @@
 <hr/>
 <form id="searchForm" method="post" action="<?php echo current_url(); ?>">
   <div class="row">
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
       <label>เลขที่เอกสาร</label>
       <input type="text" class="form-control input-sm text-center search" name="code" value="<?php echo $code; ?>" />
     </div>
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
       <label>ลูกค้า</label>
       <input type="text" class="form-control input-sm text-center search" name="customer" value="<?php echo $customer; ?>" />
     </div>
-		<div class="col-sm-2 padding-5">
+		<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 padding-5">
       <label>โซน</label>
       <input type="text" class="form-control input-sm text-center search" name="zone" value="<?php echo $zone; ?>" />
     </div>
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
     	<label>ช่องทางการรับ</label>
 			<select class="form-control input-sm" name="is_wms" onchange="getSearch()">
 				<option value="all">ทั้งหมด</option>
@@ -34,7 +34,7 @@
 				<option value="0" <?php echo is_selected('0', $is_wms); ?>>Warrix</option>
 			</select>
     </div>
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
       <label>สถานะ</label>
       <select class="form-control input-sm" name="status" onchange="getSearch()">
   			<option value="all">ทั้งหมด</option>
@@ -45,7 +45,7 @@
   		</select>
     </div>
 
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
       <label>การดึงยอด</label>
       <select class="form-control input-sm" name="valid" onchange="getSearch()">
   			<option value="all">ทั้งหมด</option>
@@ -54,24 +54,24 @@
   		</select>
     </div>
 
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 padding-5">
       <label>เลขที่อ้างอิง</label>
       <input type="text" class="form-control input-sm text-center search" name="consign_code" value="<?php echo $consign_code; ?>" />
     </div>
-    <div class="col-sm-2 padding-5">
+    <div class="col-lg-2 col-md-2-harf col-sm-3 col-xs-6 padding-5">
       <label>วันที่</label>
-      <div class="input-daterange input-group">
+      <div class="input-daterange input-group width-100">
         <input type="text" class="form-control input-sm width-50 from-date" name="from_date" id="fromDate" value="<?php echo $from_date; ?>" />
         <input type="text" class="form-control input-sm width-50" name="to_date" id="toDate" value="<?php echo $to_date; ?>" />
       </div>
     </div>
-    <div class="col-sm-1 padding-5">
+    <div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-3 padding-5">
       <label class="display-block not-show">btn</label>
-      <button type="button" class="btn btn-xs btn-primary btn-block" onclick="getSearch()"><i class="fa fa-search"></i> ค้นหา</button>
+      <button type="button" class="btn btn-xs btn-primary btn-block" onclick="getSearch()">ค้นหา</button>
     </div>
-    <div class="col-sm-1 padding-5">
+    <div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-3 padding-5">
       <label class="display-block not-show">btn</label>
-      <button type="button" class="btn btn-xs btn-warning btn-block" onclick="clearFilter()"><i class="fa fa-retweet"></i> Reset</button>
+      <button type="button" class="btn btn-xs btn-warning btn-block" onclick="clearFilter()">Reset</button>
     </div>
   </div>
 </form>
@@ -87,17 +87,18 @@
 			<span class="purple">OP</span> = รอรับที่ WMS,&nbsp;
       <span class="red">CN</span> = ยกเลิก
     </p>
-    <table class="table table-striped border-1">
+    <table class="table table-striped border-1" style="min-width:1090px;">
       <thead>
         <tr>
-          <th class="width-5 text-center">ลำดับ</th>
-          <th class="width-10 text-center">วันที่</th>
-          <th class="width-10">เลขที่เอกสาร</th>
-          <th class="width-25">ลูกค้า</th>
-          <th class="width-25">โซน</th>
-          <th class="width-10">อ้างอิง</th>
-          <th class="width-5 text-center">สถานะ</th>
-          <th class=""></th>
+          <th class="fix-width-100"></th>
+          <th class="fix-width-50 text-center">สถานะ</th>
+          <th class="fix-width-40 text-center">#</th>
+          <th class="fix-width-100 text-center">วันที่</th>
+          <th class="fix-width-120">เลขที่เอกสาร</th>
+          <th class="fix-width-80 text-center">ช่องทาง</th>
+          <th class="min-width-250">ลูกค้า</th>
+          <th class="min-width-250">โซน</th>
+          <th class="fix-width-100">อ้างอิง</th>
         </tr>
       </thead>
       <tbody>
@@ -105,12 +106,15 @@
 <?php   $no = $this->uri->segment(4) + 1; ?>
 <?php   foreach($docs as $rs) : ?>
           <tr class="font-size-12" id="row-<?php $rs->code; ?>">
-            <td class="middle text-center no"><?php echo $no; ?></td>
-            <td class="middle text-center"><?php echo thai_date($rs->date_add, FALSE); ?></td>
-            <td class="middle"><?php echo $rs->code; ?></td>
-            <td class="middle"><?php echo $rs->customer_name; ?></td>
-            <td class="middle"><?php echo $rs->zone_name; ?></td>
-            <td class="middle"><?php echo $rs->consign_code; ?></td>
+            <td class="middle">
+              <button type="button" class="btn btn-minier btn-info" onclick="viewDetail('<?php echo $rs->code; ?>')"><i class="fa fa-eye"></i></button>
+              <?php if($this->pm->can_edit && $rs->status == 0 && $rs->valid == 0) : ?>
+                  <button type="button" class="btn btn-minier btn-warning" onclick="goEdit('<?php echo $rs->code; ?>')"><i class="fa fa-pencil"></i></button>
+              <?php endif; ?>
+              <?php if($this->pm->can_delete && $rs->status != 2 && $rs->valid == 0) : ?>
+                  <button type="button" class="btn btn-minier btn-danger" onclick="goDelete('<?php echo $rs->code; ?>')"><i class="fa fa-trash"></i></button>
+              <?php endif; ?>
+            </td>
             <td class="middle text-center">
               <?php if($rs->status == 2) : ?>
                 <span class="red">CN</span>
@@ -124,21 +128,30 @@
                 <span class="green">OK</span>
               <?php endif; ?>
             </td>
-            <td class="middle text-right">
-              <button type="button" class="btn btn-minier btn-info" onclick="viewDetail('<?php echo $rs->code; ?>')"><i class="fa fa-eye"></i></button>
-          <?php if($this->pm->can_edit && $rs->status == 0 && $rs->valid == 0) : ?>
-              <button type="button" class="btn btn-minier btn-warning" onclick="goEdit('<?php echo $rs->code; ?>')"><i class="fa fa-pencil"></i></button>
-          <?php endif; ?>
-          <?php if($this->pm->can_delete && $rs->status != 2 && $rs->valid == 0) : ?>
-              <button type="button" class="btn btn-minier btn-danger" onclick="goDelete('<?php echo $rs->code; ?>')"><i class="fa fa-trash"></i></button>
-          <?php endif; ?>
+            <td class="middle text-center no"><?php echo $no; ?></td>
+            <td class="middle text-center"><?php echo thai_date($rs->date_add, FALSE); ?></td>
+
+            <td class="middle">
+              <?php echo $rs->code; ?>
+              <?php if($rs->is_wms == 2 && ! empty($rs->soko_code)) : ?>
+                [<?php echo $rs->soko_code; ?>]
+              <?php endif; ?>
+              <?php if($rs->is_wms != 0 && $rs->status == 3 && $rs->wms_export == 3) : ?>
+                <span class="red">failed</span>
+              <?php endif; ?>
             </td>
+            <td class="middle text-center">
+              <?php echo ($rs->is_wms == 0 ? 'Warrix' : ($rs->is_wms == 2 ? 'Soko' : ($rs->is_wms == 1 ? 'PLC' : 'Warrix'))); ?>
+            </td>
+            <td class="middle"><?php echo $rs->customer_name; ?></td>
+            <td class="middle"><?php echo $rs->zone_name; ?></td>
+            <td class="middle"><?php echo $rs->consign_code; ?></td>
           </tr>
 <?php     $no++; ?>
 <?php   endforeach; ?>
 <?php else : ?>
         <tr>
-          <td colspan="8" class="text-center">
+          <td colspan="9" class="text-center">
             --- ไม่พบรายการ ---
           </td>
         </tr>
