e268645ba63e734d8b084b35fc4d2710ffd1219c
sutouch08 <sutouch08@gmail.com>
2025-04-08 10:48:52 +0700

    increase memort in runtime on every report

--- application/controllers/report/audit/Order_channels_items.php
+++ application/controllers/report/audit/Order_channels_items.php
@@ -33,7 +33,7 @@ class Order_channels_items extends PS_Controller
 
   public function do_export()
   {
-    ini_set('memory_limit','512M'); // This also needs to be increased in some cases. Can be changed to a higher value as per need)
+    ini_set('memory_limit','2048M'); // This also needs to be increased in some cases. Can be changed to a higher value as per need)
     ini_set('sqlsrv.ClientBufferMaxKBSize','524288'); // Setting to 2048M
     ini_set('sqlsrv.client_buffer_max_kb_size','524288'); // Setting to 512M - for pdo_sqlsrv
 
