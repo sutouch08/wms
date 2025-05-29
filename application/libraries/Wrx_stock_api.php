7ebcbb6b053f7b2c3aabbc80bd00fc26e8e27803
sutouch08 <sutouch08@gmail.com>
2025-05-29 12:17:27 +0700

    test logs stock api

--- application/libraries/Wrx_stock_api.php
+++ application/libraries/Wrx_stock_api.php
@@ -117,7 +117,7 @@ class Wrx_stock_api
           $cmd = "curl -X POST $apiUrl " .
           "-H 'Content-Type: application/json' " .
           "-H 'Authorization: Bearer {$this->api['WRX_API_CREDENTIAL']}' " .
-          "-d '" . $json . "' " .
+          "-d '" . addslashes($json) . "' " .
           "> /dev/null 2>&1 &";
           exec($cmd);
           return TRUE;
@@ -275,7 +275,7 @@ class Wrx_stock_api
           $cmd = "curl -X POST {$apiUrl} "
           ."-H 'Content-Type:application/json' "
           ."-H 'Authorization:Bearer {$this->api['WRX_API_CREDENTIAL']}' "
-          ."-d '" . addslashes($json) . "'";
+          ."-d '" . $json . "'";
           // ." > /dev/null 2>&1 &";
           echo $cmd ."<br/>";
           exec($cmd, $output, $exit);
