064ff72d92ea5ca6803b2b6e9416d164d54fcec8
sutouch08 <sutouch08@gmail.com>
2025-09-16 09:38:23 +0700

    auto check mkp status

--- application/controllers/auto/Auto_check_shopee_status.php
+++ application/controllers/auto/Auto_check_shopee_status.php
@@ -12,16 +12,16 @@ class Auto_check_shopee_status extends CI_Controller
   public $error;
   public $default_shop_id = "32706050";
   public $statusList = [
-    'UNPAID' => 'Order is created, buyer has not paid yet',
-    'READY_TO_SHIP' => 'Seller can arrange shipment',
-    'PROCESSED' => 'Seller has arranged shipment online and got tracking number from 3PL',
-    'RETRY_SHIP' => '3PL pickup parcel fail Need to re arrange shipment',
-    'SHIPPED' => 'The parcel has been drop to 3PL or picked up by 3PL',
-    'TO_CONFIRM_RECEIVE' => 'The order has been received by buyer',
-    'IN_CANCEL' => 'The order\'s cancelation is under processing',
-    'CANCELLED' => 'The order has been canceled',
-    'TO_RETURN' => 'The buyer requested to return the order and order\'s return is processing',
-    'COMPLETED' => 'The order has been completed',
+    'UNPAID' => 'UNPAID : Order is created, buyer has not paid yet',
+    'READY_TO_SHIP' => 'READY_TO_SHIP : Seller can arrange shipment',
+    'PROCESSED' => 'PROCESSED : Seller has arranged shipment online and got tracking number from 3PL',
+    'RETRY_SHIP' => 'RETRY_SHIP : 3PL pickup parcel fail Need to re arrange shipment',
+    'SHIPPED' => 'SHIPPED : The parcel has been drop to 3PL or picked up by 3PL',
+    'TO_CONFIRM_RECEIVE' => 'TO_CONFIRM_RECEIVE : The order has been received by buyer',
+    'IN_CANCEL' => 'IN_CANCEL : The order\'s cancelation is under processing',
+    'CANCELLED' => 'CANCELLED : The order has been canceled',
+    'TO_RETURN' => 'TO_RETURN : The buyer requested to return the order and order\'s return is processing',
+    'COMPLETED' => 'COMPLETED : The order has been completed',
   ];
 
   public function __construct()
