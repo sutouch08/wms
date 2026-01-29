d28c015653d476ec8a0873f8ad7f9a705ad9a496
sutouch08 <sutouch08@gmail.com>
2026-01-29 09:59:03 +0700

    fix report summary stock zone

--- application/models/report/inventory/Summary_stock_zone_model.php
+++ application/models/report/inventory/Summary_stock_zone_model.php
@@ -10,37 +10,23 @@ class Summary_stock_zone_model extends CI_Model
   {
     $zone = $whsCode.'-'.$rowCode;
 
+    $qr  = "SELECT SUM(Qty) AS Qty, BinName ";
+    $qr .= "FROM (SELECT SUM(Q.OnHandQty) AS Qty, B.SL1Code AS BinName FROM OIBQ Q INNER JOIN OBIN B ON Q.BinAbs = B.AbsEntry ";
+    $qr .= "WHERE Q.WhsCode = '{$whsCode}' AND B.BinCode LIKE '{$zone}%' GROUP BY Q.BinAbs, B.SL1Code) AS S ";
+    $qr .= "GROUP BY BinName ";
+
+
     if($option == 'E')
     {
-      $qr  = "SELECT SUM(Q.OnHandQty) AS Qty, B.SL1Code AS BinName ";
-      $qr .= "FROM OIBQ Q INNER JOIN OBIN B ON Q.BinAbs = B.AbsEntry ";
-      $qr .= "WHERE Q.WhsCode = '{$whsCode}' ";
-      $qr .= "AND B.BinCode LIKE '{$zone}%' ";
-      $qr .= "AND Q.OnHandQty = 0 ";
-      $qr .= "GROUP BY Q.BinAbs, B.SL1Code ";
-      $qr .= "ORDER BY B.SL1Code ASC";
+      $qr .= "HAVING SUM(Qty) = 0 ";
     }
 
     if($option == 'S')
     {
-      $qr  = "SELECT SUM(Qty) AS Qty, BinName ";
-      $qr .= "FROM (SELECT SUM(Q.OnHandQty) AS Qty, B.SL1Code AS BinName FROM OIBQ Q INNER JOIN OBIN B ON Q.BinAbs = B.AbsEntry ";
-      $qr .= "WHERE Q.WhsCode = '{$whsCode}' ";
-      $qr .= "AND B.BinCode LIKE '{$zone}%' ";
-      $qr .= "GROUP BY Q.BinAbs, B.SL1Code) AS S ";
-      $qr .= "GROUP BY BinName HAVING SUM(Qty) < 1000 ";
-      $qr .= "ORDER BY BinName ASC";
+      $qr .= "HAVING SUM(Qty) < 1000 ";
     }
 
-    if($option == 'A')
-    {
-      $qr  = "SELECT SUM(Q.OnHandQty) AS Qty, B.SL1Code AS BinName ";
-      $qr .= "FROM OIBQ Q INNER JOIN OBIN B ON Q.BinAbs = B.AbsEntry ";
-      $qr .= "WHERE Q.WhsCode = '{$whsCode}' ";
-      $qr .= "AND B.BinCode LIKE '{$zone}%' ";
-      $qr .= "GROUP BY Q.BinAbs, B.SL1Code ";
-      $qr .= "ORDER BY B.SL1Code ASC";
-    }
+    $qr .= "ORDER BY BinName ASC";
 
     $rs = $this->ms->query($qr);
 
