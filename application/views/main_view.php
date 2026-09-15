8dd28aa2e321c2cdefa5d9196b99796c78c91a27
sutouch08 <sutouch08@gmail.com>
2026-09-15 12:55:48 +0700

    main view

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -27,7 +27,7 @@
 
 	<div class="col-lg-1-harf col-md-2 col-sm-2 col-xs-6 padding-5">
 		<label class="display-block not-show">stock</label>
-		<button type="button" class="btn btn-xs btn-primary btn-block" onclick="getSearch()">ตรวจสอบสต็อก</button>
+		<button type="button" class="btn btn-xs btn-primary btn-block" onclick="getStockSearch()">ตรวจสอบสต็อก</button>
 	</div>
 	<div class="col-lg-1-harf col-md-2 col-sm-2 col-xs-6 padding-5">
 		<label class="display-block not-show">stock</label>
@@ -147,7 +147,7 @@
 
 
 
-	function getSearch() {
+	function getStockSearch() {
 		var searchText = $.trim($('#search-text').val());
 		var warehouse = $('#warehouse').val();
 		var color = $('#color').val();
